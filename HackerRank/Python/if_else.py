fe508b975267c801dc80fdbb925811e776b24288
Jacque Carvalho <ds.jacque@gmail.com>
2020-04-20 13:05:18 -0300

    Update if_else.py

--- HackerRank/Python/if_else.py
+++ HackerRank/Python/if_else.py
@@ -1,7 +1,9 @@
 #!/bin/python3
 
+
 N = int(input())
 
+# option 1
 if (N in range (1, 100) and N % 2 != 0):
     print("Weird")
 elif (N in range (2, 6) or N % 2):
@@ -10,3 +12,9 @@ elif (N in range (6,21) or N % 2):
     print('Weird')
 else:
     print('Not Weird')
+
+# option 2
+if (n %2) == 0 and n in range(6,20) or (n %2) != 0:
+    print("Weird")
+else:
+    print("Not Weird")
