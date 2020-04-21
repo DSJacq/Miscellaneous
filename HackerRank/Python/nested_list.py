4b8fae69887b61bf24802896f46734d7f297b06b
Jacque Carvalho <ds.jacque@gmail.com>
2020-04-21 09:55:31 -0300

    Create nested_list.py

---
+++ HackerRank/Python/nested_list.py
@@ -0,0 +1,17 @@
+students = [['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39]]
+
+marksheet=[]
+scorelist=[]
+
+if __name__ == '__main__':
+        for _ in range(int(input())):
+                name = input()
+                score = float(input())
+                marksheet += [[name,score]]
+                scorelist += [score]
+        b = sorted(list(set(scorelist)))[1] 
+
+        for a, c in sorted(marksheet):
+             if c == b:
+                    print(a)
+                    
