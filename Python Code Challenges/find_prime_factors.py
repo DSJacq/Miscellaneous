f239f483f1282509a6892ea855e81f8925b5a246
Jacque Carvalho <ds.jacque@gmail.com>
2020-05-06 15:22:33 -0300

    Update find_prime_factors.py

--- Python Code Challenges/find_prime_factors.py
+++ Python Code Challenges/find_prime_factors.py
@@ -1,1 +1,10 @@
-
+def get_prime_factors(N):
+    factors = list()
+    divisor = 2
+    while(divisor <= N):
+        if (N % divisor) == 0:
+            factors.append(divisor)
+            N = N/divisor
+        else:
+            divisor += 1
+    return factors
