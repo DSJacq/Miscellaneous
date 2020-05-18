48ae217307b5f46ffc2d17b67a195c6bc1e1649d
Jacque Carvalho <ds.jacque@gmail.com>
2020-05-18 13:29:05 -0300

    Update find_string.py

--- HackerRank/Python/find_string.py
+++ HackerRank/Python/find_string.py
@@ -1,3 +1,5 @@
+# Option 1
+
 def count_substring(string, sub_string):
     return len([i for i in range(len(string)) if string[i:i+len(sub_string)]==sub_string])
 
@@ -7,3 +9,23 @@ if __name__ == '__main__':
     
     count = count_substring(string, sub_string)
     print(count)
+    
+    # Option 2
+    
+    A = raw_input().strip()
+x = raw_input().strip()
+
+count = 0
+for i in range(len(A) - len(x) + 1):
+    if A[i:i+len(x)] == x:
+        count += 1
+print count
+
+# Option 3
+
+import re
+a = raw_input()
+b = raw_input()
+match = re.findall('(?='+b+')',a)
+print len(match)
+
