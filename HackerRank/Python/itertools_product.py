48e103665d76b8c00c50c3954b2e10335c22cd96
Jacque Carvalho <ds.jacque@gmail.com>
2021-02-17 10:50:23 -0300

    Update itertools_product.py

--- HackerRank/Python/itertools_product.py
+++ HackerRank/Python/itertools_product.py
@@ -1,4 +1,9 @@
-from itertools import product
+import itertools
 
-print(list(product([1,2,3],repeat = 2)))
-print(list(product([1,2],[3,4])))
+print(list(itertools.product([1,2,3],repeat = 2)))
+print(list(itertools.product([1,2],[3,4])))
+
+A = [int(x) for x in input().split()]
+B = [int(y) for y in input().split()]
+
+print(*itertools.product(A, B))
