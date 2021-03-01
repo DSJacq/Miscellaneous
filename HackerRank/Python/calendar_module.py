4f76d4f9931b92cfcc8888914e7bbed63c722575
Jacque Carvalho <ds.jacque@gmail.com>
2021-03-01 19:10:18 -0300

    Create calendar_module.py

---
+++ HackerRank/Python/calendar_module.py
@@ -0,0 +1,8 @@
+import calendar
+
+print(calendar.TextCalendar(firstweekday=6).formatyear(2021))
+
+# bring the day of the week
+n1,n2,n3=map(int,input().split())
+print((calendar.day_name[calendar.weekday(n3,n1,n2)]).upper())
+# input 08 12 2021
